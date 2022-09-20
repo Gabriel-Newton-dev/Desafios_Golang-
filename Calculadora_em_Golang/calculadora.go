6be5fea46db3735240df7b4349be416927d9f163
Gabriel <gabriel@MacBook.local>
2022-09-20 00:24:31 -0700

    Calculadora em Golang

---
+++ Calculadora_em_Golang/calculadora.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var number1 int
+var number2 int
+var operadores string
+
+func main() {
+
+	fmt.Printf("Digite o primeiro número por favor: ")
+	fmt.Scan(&number1)
+
+	fmt.Print("Digite o segundo número: ")
+	fmt.Scan(&number2)
+
+	fmt.Print("Qual operação deseja fazer (+ | - | *  | / | %) :")
+	fmt.Scan(&operadores)
+
+	switch operadores {
+	case "+":
+		fmt.Println(number1 + number2)
+	case "-":
+		fmt.Println(number1 - number2)
+	case "*":
+		fmt.Println(number1 * number2)
+	case "/":
+		fmt.Println(number1 / number2)
+	case "%":
+		fmt.Println(number1 % number2)
+	}
+
+}
