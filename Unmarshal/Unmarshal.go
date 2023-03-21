e56f148d500a373240fab6ad07c7c313d0eb9374
Gabriel <gabriel@MacBook.local>
2023-03-21 10:13:11 -0700

    Unmarshal

--- Unmarshal/Unmarshal.go
+++ Unmarshal/Unmarshal.go
@@ -7,24 +7,22 @@ import (
 )
 
 type Person struct {
-	Name         string  `json:"name"`
-	Surname      string  `json:"surname"`
-	Age          int     `json:"age"`
-	Profession   string  `json:"profession`
-	BankAccounts float64 `json:"bankAccounts`
+	Name       string `json:"name"`
+	Surname    string `json:"surname"`
+	Age        int    `json:"age"`
+	Profession string `json:"profession"`
+	//BankAccounts float64 `json:"bankAccounts"`
 }
 
 func main() {
 
-	Json := []byte(`{"Name":"James","Surname":"Bond","Age":51,"Profession":"Secret Agent","BankAccount":5000000.56}`)
+	RonaldinhoGaucho := []byte(`{"Name": "Ronaldinho", "Surname": "Gaúcho", "Age": 49, "Profession": "Jogador de futebol"}`)
 
-	RonaldinhoGaucho := []byte{"Name": "Ronaldinho", "Surname": "Gaúcho", "Age": 49, "Profession": "Jgador de futebol", "BankAccounts": "5000000"}
-
-	var jameBond Person
-	err := json.Unmarshal(Json, &jameBond)
+	var Ronaldinho Person
+	err := json.Unmarshal(RonaldinhoGaucho, &Ronaldinho)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(jameBond)
+	fmt.Println(Ronaldinho)
 
 }
